04ae3cd012eec0c751946edda7e900bef32e9f33
Lígia Andrade <andradeligiamp@gmail.com>
2026-08-08 09:56:20 -0300

    Criação  e desenvolvimento do Component Button .

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom'
+import { cores } from "../../styles";
+
+
+export const ButtonContainer = styled.button`
+    border: 2px solid ${cores.branco};
+    color:${cores.branco} ;
+    background-color: transparent;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 8px 16px;
+`
+export const BotaoSecundario = styled(ButtonContainer)`
+    background-color: red;
+`
+
+
+export const  ButtonLink = styled(Link)`
+    border: 2px solid ${cores.branco};
+    color:${cores.branco} ;
+    background-color: transparent;
+    font-size: 16px;
+    font-weight: bold;
+    padding: 8px 16px;
+    text-decoration: none;
+
+`
